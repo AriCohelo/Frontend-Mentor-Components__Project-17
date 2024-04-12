c4babe186c4f7f91e9508f4b5e2f6fc5072f95d5
AriCohelo <ariel.cohelo@gmail.com>
2024-04-12 22:13:28 +0200

    finished the logic for mail validation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './main.scss';
+let email = document.getElementById('email');
+let submitButton = document.querySelector('.card__info-email-button');
+let emailForm = document.querySelector('.card__info-email');
+
+let emailError = document.querySelector('.card__info-email-error');
+
+submitButton.addEventListener('click', (e) => {
+	if (!email.value) return;
+	if (!email.checkValidity()) {
+		e.preventDefault();
+		emailError.style.display = 'grid';
+		emailForm.style.display = 'none';
+	}
+});
